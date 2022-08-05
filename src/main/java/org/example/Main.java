903fe147c3a68327ed9799aa781e8791ab267a80
Sergey Ryabinin <ryabinin.sergey@gmail.com>
2022-08-05 04:18:41 +0300

    well formed checking

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,7 +1,27 @@
+/**
+ * License - none
+ */
 package org.example;
 
-public class Main {
-    public static void main(String[] args) {
+
+/**
+ * Main class.
+ * @author VraGos
+ */
+public final class Main {
+
+    /**
+     * Main constructor.
+     */
+    private Main() {
+        System.out.print("Start application");
+    }
+
+    /**
+     * Start appication.
+     * @param args this args not used in progrem.
+     */
+    public static void main(final String... args) {
         System.out.println("Hello world!");
     }
 }
